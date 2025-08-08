f2d8e516eab975bfb3fecd1a11cd7a9e8f97bc56
Adrielly0124 <drika.dantas24@gmail.com>
2025-08-08 11:25:52 -0300

    08/08/2025

---
+++ JsonConvert/json_shared_preferences/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp>{
+  bool temaEscuro = false;
+  String nomeUsuario = "";
+
+  @override
+  void initState() {
+    super.initState();
+    carregarPreferencias();
+  }
+
+  void carregarPreferencias() async{
+    //conecta com as shared preferences e busca as informações aramazenadas
+    final prefs = await SharedPreferences.getInstance();
+    //recupera as informaç~eos do sharedPref e armazena como String (Json)
+    String? jsonString = prefs.getString('config');
+    if(jsonString != null){
+      // Transformo Json em MAP
+      Map<String,dynamic> config = json.decode(jsonString);
+      setState(() {
+        temaEscuro = config['temaEscuro'] ?? false;
+        nomeUsuario = config['nome'] ?? "";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "App de configurações",
+      //operador ternário para definir o tema
+      theme: temaEscuro ? ThemeData.dark() : ThemeData.light(),
+      home: ConfigPage()
+    );
+    }
+}
