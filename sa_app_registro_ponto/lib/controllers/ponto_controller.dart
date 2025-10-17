b5e16c4144aaee8684c60a08d2477f43e588c00e
Adrielly0124 <drika.dantas24@gmail.com>
2025-10-17 15:50:03 -0300

    17/10/2025

--- sa_app_registro_ponto/lib/controllers/ponto_controller.dart
+++ sa_app_registro_ponto/lib/controllers/ponto_controller.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../models/ponto_model.dart';
 
 class PontoController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<bool> registrarPonto(String userId) async {
+  Future<void> registrarPonto() async {
+    // Verifica permissões de localização
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
@@ -13,9 +16,11 @@ class PontoController {
         throw Exception('Permissão de localização negada.');
       }
     }
+
+    // Pega localização atual
     Position position = await Geolocator.getCurrentPosition();
-    const double localLat = -22.570889; 
-    const double localLng = -47.403854;  
+    const double localLat = -22.570889;
+    const double localLng = -47.403854;
 
     double distancia = Geolocator.distanceBetween(
       localLat,
@@ -28,14 +33,19 @@ class PontoController {
       throw Exception('Você está fora do raio de 100 metros do local de trabalho.');
     }
 
-    PontoModel ponto = PontoModel(
-      userId: userId,
+    // Usuário logado
+    final user = _auth.currentUser;
+    if (user == null) throw Exception("Usuário não autenticado.");
+
+    // Modelo de ponto
+    final ponto = PontoModel(
+      userId: user.uid,
       dataHora: DateTime.now(),
       latitude: position.latitude,
       longitude: position.longitude,
     );
 
-    await _firestore.collection('registros_ponto').add(ponto.toMap());
-    return true;
+    // Salva no Firestore
+    await _firestore.collection("pontos").add(ponto.toMap());
   }
 }
